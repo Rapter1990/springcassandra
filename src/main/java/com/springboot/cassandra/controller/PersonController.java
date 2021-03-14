e9fd059a2a8ad3e18c15885f600060b0c1462c79
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-03-14 11:45:36 +0300

    Task 8 : Update PersonController

--- src/main/java/com/springboot/cassandra/controller/PersonController.java
+++ src/main/java/com/springboot/cassandra/controller/PersonController.java
@@ -6,7 +6,6 @@ import com.springboot.cassandra.service.PersonService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
