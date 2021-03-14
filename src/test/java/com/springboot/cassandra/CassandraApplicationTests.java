94ce9dc48c9da384c4e7a20c93859c483a3b566d
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-03-14 10:46:18 +0300

    Task 1 : Initial commit

---
+++ src/test/java/com/springboot/cassandra/CassandraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.cassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CassandraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
