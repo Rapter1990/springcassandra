213cbdb6f54a8b33d943fd0a3564a36d9f377cac
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-03-14 11:22:30 +0300

    Task 5 : Create CassandraConfiguration

---
+++ src/main/java/com/springboot/cassandra/config/CassandraConfiguration.java
@@ -0,0 +1,50 @@
+package com.springboot.cassandra.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.config.SchemaAction;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+
+@Configuration
+@EnableCassandraRepositories
+public class CassandraConfiguration extends AbstractCassandraConfiguration {
+
+    @Value("${cassandra.contactpoints}")
+    private String contactPoint;
+
+    @Value("${cassandra.port}")
+    private int port;
+
+    @Value("${cassandra.keyspace}")
+    private String keyspaceName;
+
+    @Value("${cassandra.basepackages}")
+    private String basePackages;
+
+    @Override
+    protected String getKeyspaceName() {
+        return keyspaceName;
+    }
+
+    @Override
+    protected int getPort() {
+        return port;
+    }
+
+    @Override
+    protected String getContactPoints() {
+        return contactPoint;
+    }
+
+    @Override
+    public SchemaAction getSchemaAction() {
+        return SchemaAction.CREATE_IF_NOT_EXISTS;
+    }
+
+    @Override
+    public String[] getEntityBasePackages() {
+        return new String[] {basePackages};
+    }
+
+}
